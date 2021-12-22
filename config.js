5eb95976c9af7498aa9e41578ff03af7fb1eaa3d
TomokiSuwabe <45681996+TomokiSuwabe@users.noreply.github.com>
2021-12-22 13:45:08 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCjd80kJ8xlp0Yi5jwQo-I4weKANml-SFs",
+  authDomain: "teachablemachine-d7316.firebaseapp.com",
+  projectId: "teachablemachine-d7316",
+  storageBucket: "teachablemachine-d7316.appspot.com",
+  messagingSenderId: "948617562294",
+  appId: "1:948617562294:web:eecf483c90aafeb2d2be17"
+};
+firebase.initializeApp(config);
